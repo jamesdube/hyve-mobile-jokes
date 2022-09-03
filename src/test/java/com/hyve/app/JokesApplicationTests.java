0eda45a12d6a83e244ff5c53facf2a3f00fe9017
James Dube <jamesdylandube@gmail.com>
2022-09-03 05:29:49 +0200

    initial commit

---
+++ src/test/java/com/hyve/app/JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyve.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
