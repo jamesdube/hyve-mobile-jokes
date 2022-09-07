bb1bfdfa0acd31b7c204091f9ec21f3498fef65e
James Dube <jamesdylandube@gmail.com>
2022-09-07 09:26:41 +0200

    add configuration property config

--- src/main/java/com/hyve/app/config/JokesConfig.java
+++ src/main/java/com/hyve/app/config/JokesConfig.java
@@ -5,12 +5,14 @@ import com.hyve.app.convertors.impl.JokeConvertorImpl;
 import com.hyve.app.repositories.JokeRepository;
 import com.hyve.app.services.JokeService;
 import com.hyve.app.services.impl.JokeServiceImpl;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Configuration
+@EnableConfigurationProperties(JokesApiProperties.class)
 public class JokesConfig {
 
     @Bean
